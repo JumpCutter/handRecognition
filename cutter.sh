09714dce4ab85093dc9c683c9e02e84db8cead88
Andrei Parrent <drei@aparrent.com>
2019-10-12 22:33:38 -0600

    working but cutting too soon

--- cutter.sh
+++ cutter.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# #!/bin/bash
+#!/bin/bash
+# #!/bin/sh
 # #!/usr/bin/env sh
 
 ext=${1##*.}
@@ -8,12 +8,12 @@ fps=`ffmpeg -i $1 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p"`
 frame=$(echo "0/$fps" | bc -l | sed 's/^\./0\./')
 
 tempdir=$(mktemp -d tmpvids.XXXXXX)
-trap 'rm -rf "$tempdir"' 0 1 2 9 15 
+trap 'rm -rf "$tempdir"' 0 1 2 9 15
 
 ./handrecog $1 | \
 while read fr event gest; do
     [ -n $fr ] || continue
-    echo $fr $event $gest
+    echo $fr $event $gest "$(echo $fr/$fps | bc)s"
     if [ "$event" == 'end' ]; then
         frame=$fr
     elif [ "$gest" == 'up' ]; then
@@ -22,10 +22,12 @@ while read fr event gest; do
         e=$(echo "$fr   /$fps" | bc -l | sed 's/^\./0\./')
         #echo $s $e
         ffmpeg -y -i $1 -ss $s -to $e $tempdir/$vidname.$ext 2>/dev/null
-        echo "file '$tempdir/$vidname.$ext'" >> $tempdir/filelist.txt
+        echo "file '$vidname.$ext'" >> $tempdir/filelist.txt
         vidname=$((vidname+1))
     fi
 done
 
-ffmpeg -y -f concat -i filelist.txt -c copy $1 2>/dev/null
-chmod a+wx "$1"
+# ffmpeg -y -f concat -i filelist.txt -c copy $1 2>/dev/null
+ffmpeg -y -f concat -i "$tempdir/filelist.txt" -c copy "/app/test2.mov" 2>/dev/null
+# ffmpeg -y -f concat -i "$tempdir/filelist.txt" -c copy "/app/test2.mov"
+# chmod a+wx "$1"
